161c02fddda27287fffc04c29a4a0125015c468d
Matthias Wenz <mat@bowstreet.de>
2015-09-17 23:28:16 +0200

    Initial commit.

---
+++ src/main/java/net/hockeyapp/hockeysdksample/MainActivity.java
@@ -0,0 +1,106 @@
+package net.hockeyapp.hockeysdksample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.FeedbackManager;
+import net.hockeyapp.android.UpdateManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h3>License</h3>
+ * <p/>
+ * <pre>
+ * Copyright (c) 2011-2015 Bit Stadium GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
+ * @author Matthias Wenz
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // 1. Crash Reporting - forcing a crash
+
+        Button crashButton = (Button) findViewById(R.id.crash_button);
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Just some code to cause an IndexOutOfBoundsException
+                List<String> list = new ArrayList<>();
+                String fail = list.get(0);
+            }
+        });
+
+        // 2. Feedback, showing the feedback activity
+
+        Button feedbackButton = (Button) findViewById(R.id.feedback_button);
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FeedbackManager.showFeedbackActivity(MainActivity.this);
+            }
+        });
+
+        FeedbackManager.register(this);
+
+        // 3. Feedback, show Screenshot for Feedback action
+
+        Button feedbackScreenshotButton = (Button) findViewById(R.id.button_feedback_screenshot);
+        feedbackScreenshotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FeedbackManager.setActivityForScreenshot(MainActivity.this);
+            }
+        });
+
+        // 4. Enable UpdateManager
+        checkForUpdates();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkForCrashes();
+    }
+
+    private void checkForCrashes() {
+        CrashManager.register(this);
+    }
+
+    private void checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this);
+    }
+
+}
