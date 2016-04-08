62a5082fe9cd58e0061b4f2ea68bc9651ab680df
Benjamin Reimold <bereimol@microsoft.com>
2016-04-08 14:00:51 -0700

    Use public beta sdk

--- app/src/main/java/net/hockeyapp/demo/android/MainActivity.java
+++ app/src/main/java/net/hockeyapp/demo/android/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import net.hockeyapp.android.*;
+import net.hockeyapp.android.metrics.MetricsManager;
+import net.hockeyapp.android.utils.HockeyLog;
 import net.hockeyapp.android.utils.Util;
 import org.json.JSONArray;
 
@@ -132,6 +135,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        MetricsManager.register(this, getApplication());
+        HockeyLog.setLogLevel(Log.VERBOSE);
     }
 
     @Override
